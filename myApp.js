5ded6fab8a5689095c248dc408209bcf8ed62c75
Alcun <Alasdair@talkwell.world>
2022-03-15 18:20:25 +0000

    added env with uppercase config var, in /json get change json response object to uppercase if true

--- myApp.js
+++ myApp.js
@@ -1,46 +1,31 @@
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var app = express();
 
-console.log('Hello World')
+console.log("Hello World");
 
-// on get request at home serve the html file 
+// on get request at home serve the html file
 app.get("/", (req, res) => {
-    res.sendFile(absolutePath = __dirname + "/views/index.html"
-        );
-  });
+  res.sendFile((absolutePath = __dirname + "/views/index.html"));
+});
 
-  //set up middleware
-app.use(express.static(__dirname + "/public"))
+//set up middleware
+app.use(express.static(__dirname + "/public"));
 
 // serve css
 
-app.use("/public", express.static(__dirname + "/public") )
+app.use("/public", express.static(__dirname + "/public"));
 
-
+//our get route handler
 app.get("/json", (req, res) => {
-    res.json({
-        message: "Hello json"
-    })
-  });
+  //set json obj as var
+  var jsonResponse = { message: "Hello json" };
+  //if our .env var is uppercase the perform the method
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    jsonResponse.message = jsonResponse.message.toUpperCase();
+  }
+  //response object is var with uppercase applied or not
+  res.json(jsonResponse);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
